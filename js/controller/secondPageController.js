b25a832ae152797dd5b0ee16034094852c71320c
Lucas SAUVAGE <sl200103@etu.unice.fr>
2017-01-09 16:09:30 +0100

    Map 2eme page

---
+++ js/controller/secondPageController.js
@@ -0,0 +1,39 @@
+app.controller("secondCtrl", function($scope){
+
+
+    /*
+     // ------------ MAP -------------
+     // Default position
+     var centerpos = new google.maps.LatLng(43.6176162, 7.0664266);
+     var infowindow = new google.maps.InfoWindow();
+
+     // default options for the google map
+     var optionsGmaps = {
+     center:centerpos,
+     navigationControlOptions: {style: google.maps.NavigationControlStyle.SMALL},
+     mapTypeId: google.maps.MapTypeId.ROADMAP,
+     zoom: 15
+     };
+
+     // Init map object
+     var map = new google.maps.Map(document.getElementById("map"), optionsGmaps);
+
+     for(var i = 0; i < lat2.length; i++) {
+
+     // Make new object LatLng for Google Maps
+     console.log(lat2[i] + " "+ lg2[i]);
+     var latlng = new google.maps.LatLng(lat2[i], lg2[i]);
+
+     // Add a marker at position
+     var marker = new google.maps.Marker({
+     position: latlng,
+     map: map,
+     title:"You are here",
+     });
+
+     makeInfoWindowEvent(map, infowindow, name, marker);
+     */
+});
+
+
+
